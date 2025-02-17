8ec9364c2c7fd60520b91edcf838e80a23a27bd0
F-Shouter <yukari.yamasaki1809@gmail.com>
2025-02-17 13:11:50 -0300

    code added

--- Sprint Aluno 3.sql
+++ Sprint Aluno 3.sql
@@ -14,5 +14,9 @@ CREATE TABLE sensor (
 	idSensor INT PRIMARY KEY auto_increment,
     tipo VARCHAR(30) NOT NULL, -- não pode preencher vazio
     umidade DOUBLE,
-    temperatura DOUBLE
+    temperatura DOUBLE,
+    dtManutencao DATETIME,
+    statusSensor VARCHAR(40),
+    CONSTRAINT chkStatus 
+		CHECK(statusSensor IN ('Ativo', 'Inativo', 'Manutenção')) -- constraint significa restrição
 );
