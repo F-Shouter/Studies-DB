64cad1a90f538961e1351bbf45c5b776d4caea93
F-Shouter <yukari.yamasaki1809@gmail.com>
2025-02-17 13:19:13 -0300

    code added

--- Sprint Aluno 3.sql
+++ Sprint Aluno 3.sql
@@ -10,6 +10,7 @@
 SHOW DATABASES;
 USE sprint1;
 SHOW TABLES;
+
 CREATE TABLE sensor (
 	idSensor INT PRIMARY KEY auto_increment,
     tipo VARCHAR(30) NOT NULL, -- não pode preencher vazio
@@ -20,3 +21,12 @@ CREATE TABLE sensor (
     CONSTRAINT chkStatus 
 		CHECK(statusSensor IN ('Ativo', 'Inativo', 'Manutenção')) -- constraint significa restrição
 );
+
+INSERT INTO sensor VALUES 
+	(DEFAULT, 'DHT11', '2025-02-17 13:14:00', 'Pendente');
+    
+INSERT INTO sensor (tipo, dtManutencao, statusSensor) VALUES 
+	('DAF11', '2026-01-17 13:00:00', 'Ativo');
+    
+SELECT * FROM sensor;
+    
